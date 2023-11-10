196f20a95d5d945c31341e8721e19257f98a9045
leonid <leonid_voronkov@inbox.ru>
2023-11-10 16:13:24 +0400

    Решение задач №9

---
+++ 9/59778.py
@@ -0,0 +1,14 @@
+f = open('59778.txt')
+count = 0
+for line in f:
+    nums = line.split()
+    nums = list(map(int, nums))
+    a = set(nums)
+    average = 
+    if a <= 3 and :
+        count += 1
+
+
+
+
+
